65f10a4060ab18a1dbb8a0dcb22f7c2c00ab7a44
j-andrejeva <jelizaveta.and@gmail.com>
2026-05-08 16:12:53 +0700

    Update pipeline for China dataset

--- code/01_DGE_analysis.R
+++ code/01_DGE_analysis.R
@@ -14,7 +14,8 @@ suppressWarnings({
 
 # 2. Load and prepare counts
 cat("Loading data...\n")
-count_data <- read_csv(here("data", "israel_transcript_tpm.csv"), show_col_types = FALSE) %>%
+current_dataset <- "china_transcript_tpm.csv" # тут надо менять
+count_data <- read_csv(here("data", current_dataset), show_col_types = FALSE) %>%
   group_by(gene_name, sample_id) %>%
   summarise(tpm = sum(tpm), .groups = 'drop') %>%
   pivot_wider(names_from = sample_id, values_from = tpm, values_fill = 0) %>%
@@ -24,18 +25,26 @@ count_data <- read_csv(here("data", "israel_transcript_tpm.csv"), show_col_types
 count_samples <- colnames(count_data)
 
 # 3. Load and prepare metadata
-metadata <- read.csv(here("data", "Israel_metadata.csv"), row.names = "pn_ID")
+current_metadata <- "china_metadata.csv" # тут надо менять
+needed_cols <- c("Patient_group", "Age", "Gender", "CRP_mg_L", "Calprotectin_ug_g")
+metadata <- read.csv(here("data", current_metadata), row.names = "pn_ID")
+metadata <- metadata[, intersect(names(metadata), needed_cols)]
 
 metadata <- metadata %>%
   mutate(
+    Patient_group = case_when(
+      Patient_group == "China CD" ~ "China_CD",
+      grepl("China", Patient_group) ~ "China_control", # Все остальные, где есть слово China
+      TRUE ~ as.character(Patient_group)             
+    ),
     Patient_group = as.factor(Patient_group),
     Gender = as.factor(Gender),
     Age_merged = case_when(
-      Age %in% c("15_19", "20_29") ~ "15_29",
-      Age %in% c("60_69", "70_79") ~ "60_79",
+      Age %in% c("15-19", "20-29") ~ "15-29",  
+      Age %in% c("60-69", "70-79") ~ "60-79",
       TRUE ~ as.character(Age)
     ),
-    Age = factor(Age_merged, levels = c("15_29", "30_39", "40_49", "50_59", "60_79"))
+    Age = factor(Age_merged, levels = c("15-29", "30-39", "40-49", "50-59", "60-79"))
   )
 
 # Clean NAs and synchronize samples
@@ -45,6 +54,8 @@ common_samples <- intersect(colnames(count_data), rownames(metadata))
 count_data <- count_data[, common_samples]
 metadata <- metadata[common_samples, ]
 
+metadata <- droplevels(metadata) # удаляем уровни, для которых не осталось данных после объединения
+
 # 4. Differential Gene Expression (edgeR)
 cat("Running edgeR...\n")
 y <- DGEList(counts = count_data, group = metadata$Patient_group)
@@ -52,8 +63,8 @@ y <- DGEList(counts = count_data, group = metadata$Patient_group)
 # Calculate means for output
 mean_expr <- data.frame(
   gene_id = rownames(count_data),
-  mean_Israel_CD = rowMeans(count_data[, metadata$Patient_group == "Israel_CD"]),
-  mean_Israel_control = rowMeans(count_data[, metadata$Patient_group == "Israel_control"])
+  mean_China_CD = rowMeans(count_data[, metadata$Patient_group == "China_CD"]),
+  mean_China_control = rowMeans(count_data[, metadata$Patient_group == "China_control"])
 )
 
 # Filtering and normalization
@@ -68,7 +79,7 @@ colnames(design) <- make.names(colnames(design))
 y <- estimateDisp(y, design, robust = TRUE)
 fit <- glmQLFit(y, design)
 
-contrast <- makeContrasts("Patient_groupIsrael_CD - Patient_groupIsrael_control", levels = design)
+contrast <- makeContrasts("Patient_groupChina_CD - Patient_groupChina_control", levels = design)
 qlf <- glmQLFTest(fit, contrast = contrast)
 
 # 5. Assemble results
@@ -85,12 +96,21 @@ sig_genes <- results %>%
 # 6. Save results for downstream scripts
 cat("Saving results...\n")
 
-write.csv(results, here("results", "full_dge_results.csv"), row.names = TRUE)
-write.csv(sig_genes, here("results", "significant_genes.csv"), row.names = TRUE)
+# Определяем путь к папке
+results_dir <- here("results", "China_results")
+
+# Если папки нет, создаем её
+if (!dir.exists(results_dir)) {
+  dir.create(results_dir, recursive = TRUE)
+  cat("Created directory:", results_dir, "\n")
+}
+
+write.csv(results, file.path(results_dir, "01_full_dge_results.csv"), row.names = TRUE)
+write.csv(sig_genes, file.path(results_dir, "01_significant_genes.csv"), row.names = TRUE)
 
 deg_counts <- count_data[rownames(sig_genes), ]
-saveRDS(deg_counts, here("results", "deg_counts_for_wgcna.rds"))
-saveRDS(metadata, here("results", "cleaned_metadata.rds"))
-saveRDS(count_data, here("results", "full_count_matrix.rds"))
+saveRDS(deg_counts, file.path(results_dir, "01_deg_counts_for_wgcna.rds"))
+saveRDS(metadata, file.path(results_dir, "01_cleaned_metadata.rds"))
+saveRDS(count_data, file.path(results_dir, "01_full_count_matrix.rds"))
 
 cat("Script 01 completed successfully!\n")
